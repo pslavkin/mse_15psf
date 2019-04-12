0e370702452e157b3d01b2d6735db41382d40902
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-04-12 00:02:14 -0300

    ifalta poco y lo termino...
    ya me quedan poquitos puntos del 4 y lo liquido

--- 4a.py
+++ 4a.py
@@ -8,16 +8,22 @@ N  = 1024
 fs = 1024
 a0 = 1
 f0 = 9*fs//N
+f1 = 8*fs//N
 
 dft_c  = dft_class              (     )
 sg     = signal_generator_class (     )
-pl     = plotter_class          ( 1,2 )
-graph  = 0;
+pl     = plotter_class          ( 2,1 )
 ans    = np.zeros(3)
 
-signal ,time = sg.signal_sin_cycles ( fs ,f0 ,a0 ,N,1,1)
+signal  ,time  = sg.signal_sin_cycles ( fs ,f0 ,a0 ,N ,0 ,1)
+signal2 ,time  = sg.signal_sin_cycles ( fs ,f1 ,a0 ,N ,2 ,1)
+signal=signal+signal2
+z=np.zeros(10000)
+N+=10000
+signal=np.concatenate((signal,z),axis=0)
+
 fft    ,freq = dft_c.abs            ( fs ,N  ,signal      );
-pl.plot_signal ( 1 ,time ,signal ,'seno %.2f hz N=%d' %((f0),N) ,'tiempo'  ,'volts' ,trace='-' )
+pl.plot_signal ( 1 ,time ,signal  ,'seno %.2f hz N=%d' %((f0),N) ,'tiempo'  ,'volts' ,trace='-' )
 ans[0] = dft_c.power_total ( fs, fft ,f0 )
 ans[1] = dft_c.power_bin   ( fs, fft ,f0 )
 ans[2] = dft_c.max_bin     ( fs, fft     )
