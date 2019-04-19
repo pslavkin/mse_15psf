d16a88c921963fde6446a039a26d84eec67e67ef
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-04-19 20:28:57 -0300

    arranco ej4, levanto el .map y lo grafico
    ya muestro en pantalla el .mat. al pedo la complicacion de mandarlo en
    ese formato.. tranquilamente podrian haberlo mandado separados y hubiera
    tenido 2hs mas de vida para hacer filtros.. porque convertir un .map en
    python es raro cuanto menos..
    como sea, ya lo levante y se ve todo correctamente,
    arranco con el filtrado y ver de que se trata el ejercicio

--- tp2/1a.py
+++ tp2/1a.py
@@ -10,12 +10,19 @@ np.set_printoptions(formatter={'float_kind':float_formatter})
 
 pl = plotter_class ( 2,2 )
 
-b = np.array([1/3,1/3,1/3])
-a = np.array([1,0,0])
+b = np.array([1,0,0,0,-1])
+a = np.array([1,0,0,0,0])
 
 ww  ,hh = sig.freqz(b,a);
 pl.plot_signal ( 1 ,ww ,np.abs(hh)   ,f'Respuesta en frec {b}' ,'F=2*pi*f/fs' ,'Modulo [veces]'  ,trace='-' )
 pl.plot_signal ( 3 ,ww ,np.angle(hh) ,'Respuesta de fase'      ,'F=2*pi*f/fs' ,'Fase [radianes]' ,trace='-' )
 pl.zplane(b,a,4)
+
+x=np.linspace(0,np.pi,100)
+y=np.sqrt(2*(1-np.cos(4*x)))
+y=np.arctan(np.sin(4*x)/(1-np.cos(4*x)))
+pl.plot_signal ( 2 ,x ,y ,'fase'      ,'F=2*pi*f/fs' ,'Fase [radianes]' ,trace='-' )
+
 pl.plot_show()
+
 
